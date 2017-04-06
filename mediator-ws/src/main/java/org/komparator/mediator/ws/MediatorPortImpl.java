5512b9bc6e769143821e1840d47360f7264e755f
nowitworks <joao.pinto.silveira@gmail.com>
2017-04-06 15:36:05 +0100

    Lab Web Services II done and tested

--- mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
+++ mediator-ws/src/main/java/org/komparator/mediator/ws/MediatorPortImpl.java
@@ -4,14 +4,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.jws.WebService;
+
 import org.komparator.supplier.ws.cli.SupplierClient;
 import org.komparator.supplier.ws.cli.SupplierClientException;
 
 import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINamingException;
 
-// TODO annotate to bind with WSDL
-// TODO implement port type interface
-public class MediatorPortImpl {
+@WebService(
+		endpointInterface = "org.komparator.mediator.ws.MediatorPortType", 
+		wsdlLocation = "mediator.wsdl", 
+		name = "MediatorWebService", 
+		portName = "MediatorPort", 
+		targetNamespace = "http://ws.mediator.komparator.org/", 
+		serviceName = "MediatorService"
+)
+public class MediatorPortImpl implements MediatorPortType{
 
 	// end point manager
 	private MediatorEndpointManager endpointManager;
@@ -22,13 +30,37 @@ public class MediatorPortImpl {
 
 	// Main operations -------------------------------------------------------
 	
-    // TODO
+	@Override
+	public List<ItemView> getItems(String productId) throws InvalidItemId_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public List<ItemView> searchItems(String descText) throws InvalidText_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public void addToCart(String cartId, ItemIdView itemId, int itemQty) throws InvalidCartId_Exception,
+			InvalidItemId_Exception, InvalidQuantity_Exception, NotEnoughItems_Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public ShoppingResultView buyCart(String cartId, String creditCardNr)
+			throws EmptyCart_Exception, InvalidCartId_Exception, InvalidCreditCard_Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
     
 	// Auxiliary operations --------------------------------------------------	
 	
 	
-    public String ping(){
+    public String ping(String arg0){
     	Collection<String> suppliers;
     	//É para fazer try catch ou throws? !!!!!!!!!!!!!!!!!!! TODO
     	try{
@@ -58,6 +90,24 @@ public class MediatorPortImpl {
     	return result;
     	
     }
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<CartView> listCarts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ShoppingResultView> shopHistory() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 	// View helpers -----------------------------------------------------
