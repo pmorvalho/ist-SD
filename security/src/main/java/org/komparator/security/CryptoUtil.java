cf2418dc5735eb801961df9942758a1afccd61a9
MiraoDaSilva <mirarodrigo@hotmail.com>
2017-04-29 19:54:06 +0100

    Added Crypto Implementation and Tests

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -1,12 +1,31 @@
 package org.komparator.security;
 
-import java.io.*;
-import java.security.*;
-import javax.crypto.*;
-import java.util.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class CryptoUtil {
-
-    // TODO add security helper methods
+	
+	
+	//Este throws e inapropriado, mas e o que Eles fazem nos testes TODO
+	byte[] asymCipher(byte[] plainBytes, PublicKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+		byte[] cipherBytes = cipher.doFinal(plainBytes);
+		return cipherBytes;
+	} 
+	
+	byte[] asymDecipher(byte[] cipheredBytes, PrivateKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.DECRYPT_MODE, key);
+		byte[] decipheredBytes = cipher.doFinal(cipheredBytes);
+		return decipheredBytes;
+	}
 
 }
--- security/src/test/java/org/komparator/security/CryptoUtilTest.java
+++ security/src/test/java/org/komparator/security/CryptoUtilTest.java
@@ -1,21 +1,60 @@
 package org.komparator.security;
 
-import java.io.*;
-import java.security.*;
-import javax.crypto.*;
-import java.util.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.*;
-import static org.junit.Assert.*;
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class CryptoUtilTest {
-
+	
+	
     // static members
+	/** Plain text to digest. */
+	private final String plainText = "Trabalha-se melhor sem o Silveira!";
+	/** Plain text bytes. */
+	private final byte[] plainBytes = plainText.getBytes();
 
+	/** Asymmetric cryptography algorithm. */
+	private static final String ASYM_ALGO = "RSA";
+	/** Asymmetric cryptography key size. */
+	private static final int ASYM_KEY_SIZE = 2048;
+	/**
+	 * Asymmetric cipher: combination of algorithm, block processing, and
+	 * padding.
+	 */
+	private static final String ASYM_CIPHER = "RSA/ECB/PKCS1Padding";
+	
+	private static CryptoUtil cryptoUtil = new CryptoUtil();
+	private static KeyPairGenerator keyGen;
+	private static PublicKey publicKey;
+	private static PrivateKey privateKey;
+	
+	
+	
     // one-time initialization and clean-up
     @BeforeClass
-    public static void oneTimeSetUp() {
-        // runs once before all tests in the suite
+    public static void oneTimeSetUp() throws NoSuchAlgorithmException {
+        keyGen = KeyPairGenerator.getInstance(ASYM_ALGO);
+        keyGen.initialize(ASYM_KEY_SIZE);
+        KeyPair keyPair = keyGen.generateKeyPair();
+        
+        
+        publicKey = keyPair.getPublic();
+        privateKey = keyPair.getPrivate();
     }
 
     @AfterClass
@@ -28,7 +67,7 @@ public class CryptoUtilTest {
     // initialization and clean-up for each test
     @Before
     public void setUp() {
-        // runs before each test
+
     }
 
     @After
@@ -36,13 +75,29 @@ public class CryptoUtilTest {
         // runs after each test
     }
 
-    // tests
+    // tests, estes throws sao inapropriados TODO
     @Test
-    public void test() {
-        // do something ...
-
-        // assertEquals(expected, actual);
-        // if the assert fails, the test fails
+    public void sucess() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+    	byte[] ciphered = cryptoUtil.asymCipher(plainBytes, publicKey);
+    	byte[] deciphered = cryptoUtil.asymDecipher(ciphered, privateKey);
+     
+    	assertEquals(new String(plainBytes),new String(deciphered));
     }
+    
+    @Test(expected = BadPaddingException.class)
+    public void wrongKey() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
+    	byte[] ciphered = cryptoUtil.asymCipher(plainBytes, publicKey);
+    	PrivateKey wrongKey = keyGen.generateKeyPair().getPrivate();
+    	byte[] deciphered = cryptoUtil.asymDecipher(ciphered,wrongKey);
+    }
+    
+    @Test(expected = BadPaddingException.class)
+    public void modifiedData() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+    	byte[] ciphered = cryptoUtil.asymCipher(plainBytes, publicKey);
+    	ciphered[1]= (byte) 65;
+    	byte[] deciphered = cryptoUtil.asymDecipher(ciphered, privateKey);
+    }
+    
+    
 
 }
