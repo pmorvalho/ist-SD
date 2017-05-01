68219ea0af2b0fc2335a692cd414d0a8d19f7130
nowitworks <joao.pinto.silveira@gmail.com>
2017-05-01 17:31:35 +0100

    Added final part of security lab - still missing --Proximos Passos--

--- security/src/main/java/org/komparator/security/CryptoUtil.java
+++ security/src/main/java/org/komparator/security/CryptoUtil.java
@@ -12,19 +12,57 @@ import javax.crypto.NoSuchPaddingException;
 
 public class CryptoUtil {
 	
+	private static final String ASYM_CIPHER = "RSA/ECB/PKCS1Padding";
 	
-	//Este throws e inapropriado, mas e o que Eles fazem nos testes TODO
-	byte[] asymCipher(byte[] plainBytes, PublicKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, key);
-		byte[] cipherBytes = cipher.doFinal(plainBytes);
+	//Este throws e inapropriado, mas eh o que Eles fazem nos testes TODO
+	public static byte[] asymCipher(byte[] plainBytes, PublicKey key) throws BadPaddingException {
+			//throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		
+		Cipher cipher;
+		try {
+			cipher = Cipher.getInstance(ASYM_CIPHER);
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println("No Such Algorithm");
+			return null;
+		} catch (NoSuchPaddingException e) {
+			System.err.println("No Such Padding");
+			return null;
+		}
+		//NoSuchAlgorithmException, NoSuchPaddingException
+		
+		try {
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+		} catch (InvalidKeyException e) {
+			System.err.println("Invalid Key");
+			return null;
+		}
+		//InvalidKeyException
+		
+		byte[] cipherBytes = null;
+		try {
+			cipherBytes = cipher.doFinal(plainBytes);
+		} catch (IllegalBlockSizeException e) {
+			System.err.println("Illegal Block Size");
+//		} catch (BadPaddingException e) {
+//			System.err.println("Bad Padding");
+		}
+		//IllegalBlockSizeException, BadPaddingException
+		
 		return cipherBytes;
 	} 
 	
-	byte[] asymDecipher(byte[] cipheredBytes, PrivateKey key) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+	public static byte[] asymDecipher(byte[] cipheredBytes, PrivateKey key)
+			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException{
+		
+		Cipher cipher = Cipher.getInstance(ASYM_CIPHER);
+		//NoSuchAlgorithmException, NoSuchPaddingException
+		
 		cipher.init(Cipher.DECRYPT_MODE, key);
+		//InvalidKeyException
+		
 		byte[] decipheredBytes = cipher.doFinal(cipheredBytes);
+		//IllegalBlockSizeException, BadPaddingException
+		
 		return decipheredBytes;
 	}
 
--- security/src/test/java/org/komparator/security/CryptoUtilTest.java
+++ security/src/test/java/org/komparator/security/CryptoUtilTest.java
@@ -2,12 +2,20 @@ package org.komparator.security;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.*;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -19,12 +27,15 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+import static javax.xml.bind.DatatypeConverter.printBase64Binary;
+
 public class CryptoUtilTest {
 	
 	
     // static members
 	/** Plain text to digest. */
-	private final String plainText = "Trabalha-se melhor sem o Silveira!";
+	private final String plainText = "Build Success!";
 	/** Plain text bytes. */
 	private final byte[] plainBytes = plainText.getBytes();
 
@@ -38,11 +49,15 @@ public class CryptoUtilTest {
 	 */
 	private static final String ASYM_CIPHER = "RSA/ECB/PKCS1Padding";
 	
-	private static CryptoUtil cryptoUtil = new CryptoUtil();
+	private static final String CERT = "example.cer";
+	private static final String KEYSTORE = "example.jks";
+	private static final String KEYSTORE_PASSWORD = "1nsecure";
+	private static final String KEY_ALIAS = "example";
+	private static final String KEY_PASSWORD = "ins3cur3";
+	
 	private static KeyPairGenerator keyGen;
 	private static PublicKey publicKey;
 	private static PrivateKey privateKey;
-	
 	
 	
     // one-time initialization and clean-up
@@ -78,26 +93,58 @@ public class CryptoUtilTest {
     // tests, estes throws sao inapropriados TODO
     @Test
     public void sucess() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-    	byte[] ciphered = cryptoUtil.asymCipher(plainBytes, publicKey);
-    	byte[] deciphered = cryptoUtil.asymDecipher(ciphered, privateKey);
+    	byte[] ciphered = CryptoUtil.asymCipher(plainBytes, publicKey);
+    	byte[] deciphered = CryptoUtil.asymDecipher(ciphered, privateKey);
      
     	assertEquals(new String(plainBytes),new String(deciphered));
     }
     
     @Test(expected = BadPaddingException.class)
     public void wrongKey() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
-    	byte[] ciphered = cryptoUtil.asymCipher(plainBytes, publicKey);
+    	byte[] ciphered = CryptoUtil.asymCipher(plainBytes, publicKey);
     	PrivateKey wrongKey = keyGen.generateKeyPair().getPrivate();
-    	byte[] deciphered = cryptoUtil.asymDecipher(ciphered,wrongKey);
+    	CryptoUtil.asymDecipher(ciphered,wrongKey);
     }
     
     @Test(expected = BadPaddingException.class)
     public void modifiedData() throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
-    	byte[] ciphered = cryptoUtil.asymCipher(plainBytes, publicKey);
+    	byte[] ciphered = CryptoUtil.asymCipher(plainBytes, publicKey);
     	ciphered[1]= (byte) 65;
-    	byte[] deciphered = cryptoUtil.asymDecipher(ciphered, privateKey);
+    	CryptoUtil.asymDecipher(ciphered, privateKey);
     }
     
-    
+    @Test
+    public void sucessCertKeyStore()
+    		throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException,
+    		BadPaddingException, CertificateException, UnrecoverableKeyException, KeyStoreException, IOException {
+    	
+    	PublicKey pubKey = CertUtil.getX509CertificateFromResource(CERT).getPublicKey();
+    	PrivateKey prvKey = CertUtil.getPrivateKeyFromKeyStoreResource(KEYSTORE,KEYSTORE_PASSWORD.toCharArray(), KEY_ALIAS, KEY_PASSWORD.toCharArray());
+    	
+    	System.out.println("Text: " + plainText);
+		System.out.print("BytesHex: ");
+		System.out.println(printHexBinary(plainBytes));
+		System.out.print("Bytes64: ");
+		System.out.println(printBase64Binary(plainBytes));
+		
+    	System.out.println("\nCiphering...");
+    	byte[] ciphered = CryptoUtil.asymCipher(plainBytes, pubKey);
+    	
+    	System.out.print("Ciphered BytesHex: ");
+		System.out.println(printHexBinary(ciphered));
+		System.out.print("Ciphered Bytes64: ");
+		System.out.println(printBase64Binary(ciphered));
+    	
+    	System.out.println("\nDeciphering...");
+    	byte[] deciphered = CryptoUtil.asymDecipher(ciphered, prvKey);
+    	
+    	System.out.print("Deciphered BytesHex: ");
+		System.out.println(printHexBinary(deciphered));
+		System.out.print("Deciphered Bytes64: ");
+		System.out.println(printBase64Binary(deciphered));
+		System.out.println("Deciphered text: " + new String(deciphered));
+    	
+    	assertEquals(new String(plainBytes),new String(deciphered));
+    }
 
 }
